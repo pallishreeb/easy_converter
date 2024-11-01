1319a8b7f12cd7d296a55025c980fd10edf0b6ca
Pallishree <pallishreebehera01@gmail.com>
2024-11-01 13:15:00 +0530

    added port

--- app.py
+++ app.py
@@ -188,4 +188,6 @@ def extract_pdf_pages():
     else:
         return "Invalid file format. Please upload a PDF.", 400
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
